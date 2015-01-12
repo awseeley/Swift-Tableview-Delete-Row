7dd82fefbf159eb15312625ed3d55c894cca24c3
Andrew Seeley <awseeley@gmail.com>
2015-01-12 20:19:09 +1100

    Swift Tableview Delete Row
    Swift Tableview Delete Row

---
+++ TableViewTutorial/ViewController.swift
@@ -0,0 +1,51 @@
+
+
+
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    
+    var tableData: [String] = ["Hello", "My", "Table"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableData.append("Seemu")
+        self.tableView.reloadData()
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
+     
+        var cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("Row \(indexPath.row) selected")
+    }
+
+
+    
+    
+    
+
+
+    
+
+}
+
